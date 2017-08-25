d756418499625baed92eefa45d9faa2ef33e49d4
kellaJL <31347242+kellaJL@users.noreply.github.com>
2017-08-25 21:26:00 +0800

    Create proxy_getter.py

---
+++ proxy_getter.py
@@ -0,0 +1,7 @@
+from get_kuaidaili_ip import GetKuaiDaiLiIp
+def get_ip():
+    s = GetKuaiDaiLiIp()
+    url = 'http://www.kuaidaili.com/free/'
+    for i in range(2):
+        url1=url+'/inha/'+i+'/'
+        s.ParseAndGetInfo(url1)
