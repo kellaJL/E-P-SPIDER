617180ba39554d424bb743acccfac7d7f161add7
kellaJL <31347242+kellaJL@users.noreply.github.com>
2017-08-26 22:03:44 +0800

    Delete proxy_getter.py

--- proxy_getter.py
+++
@@ -1,7 +0,0 @@
-from get_kuaidaili_ip import GetKuaiDaiLiIp
-def get_ip():
-    s = GetKuaiDaiLiIp()
-    url = 'http://www.kuaidaili.com/free/'
-    for i in range(2):
-        url1=url+'/inha/'+i+'/'
-        s.ParseAndGetInfo(url1)
